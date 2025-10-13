ea1e7b5556a02caa69c58fd352c28177fa592981
Billy Harris <billythecorndog@gmail.com>
2025-10-12 21:04:18 -0400

    updated functions

--- butext/main.py
+++ butext/main.py
@@ -8,39 +8,23 @@ def tokenize(df, col):
   tokens= df.assign(word= df[col].str.lower().str.findall(r"\w+(?:\S?\w+)*")).explode("word")
   return tokens   #remove ["word"] to get entire dataframe
 
-def rel_freq(df , col, col2):
-    tok = tokenize(df, col)
-    df = tok[[col2, "word"]]
-    df = df.loc[ ~df["word"].isin(STOPWORDS) ]
-    group = df.groupby(col2)["word"].value_counts(normalize = True)
-    group = group[group > 0.0005]
-    group.name = "text_freq"
-    group = group.reset_index()
-    group = group.pivot(index='word', columns= col2, values='text_freq')
-    group = group.reset_index()
-    group.loc[group[group.columns[1]].isna(), group.columns[1]] = 0.0005/2
-    group.loc[group[group.columns[2]].isna(), group.columns[2]]  = 0.0005/2
-    group['rel_freq'] = group[group.columns[1]]/group[group.columns[2]]
-    group["logratio"] = np.log10(group["rel_freq"])
-    return group
+def rel_freq(df, col):
+  df = df.pivot(index='word', columns= col, values='text_freq')
+  df = df.reset_index()
+  df.loc[df[df.columns[1]].isna(), df.columns[1]] = 0.0005/2
+  df.loc[df[df.columns[2]].isna(), df.columns[2]]  = 0.0005/2
+  df['rel_freq'] = df[df.columns[1]]/df[df.columns[2]]
+  df["logratio"] = np.log10(df["rel_freq"])
+  return df
 # Let col be the column you want to tokenize and col2 be the categories you want to find rel freq between
 
-def tfidf(df , col, col2):
-  tok = tokenize(df, col)
-  df = tok[[col2, 'word']]
-  df = df.loc[ ~df["word"].isin(STOPWORDS) ]
-  counts = df.groupby(col2)["word"].value_counts()
-  counts.name = "n"
-  counts = counts.reset_index()
-  tf = df.groupby(col2)["word"].value_counts(normalize = True)
-  tf.name = "text_freq"
-  tf = tf.reset_index()
-  counts['tf'] = tf['text_freq'].values
-  doc = counts.groupby('word')[col2].count()
+def tfidf(df1 , df2, col):
+  df1['text_freq'] = df2[df2.columns[2]].values
+  doc = df1.groupby('word')[col].count()
   doc.name = 'df'
   doc = doc.reset_index()
-  N = df[col2].nunique()
+  N = df1[col].nunique()
   doc['idf'] = np.log(N / doc["df"])
   result = counts.merge(doc[["word", "idf"]], on="word")
-  result["tfidf"] = result["tf"] * result["idf"]
+  result["tf_idf"] = result['text_freq'] * result['idf']
   return result
