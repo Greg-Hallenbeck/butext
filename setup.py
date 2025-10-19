ab5d294f433fcbb276e877b125245601c854f3d9
Billy Harris <billythecorndog@gmail.com>
2025-10-19 18:21:56 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from setuptools import setup, find_packages
+with open('README.md') as f:
+    readme = f.read()
 setup(
     name='butext',
     version='0.3.4',
     description='A collection of functions related to Lexical Analysis',
+    long_description=readme,
     packages=find_packages(),
     install_requires=[
         'pandas',
