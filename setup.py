021c3ba4636f5fd2cceebd882646c96c34f093dc
Billy Harris <billythecorndog@gmail.com>
2025-10-19 18:06:17 -0400

    testing

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from setuptools import setup, find_packages
 setup(
     name='butext',
-    version='0.3.3',
+    version='0.3.4',
+    description='A collection of functions related to Lexical Analysis',
     packages=find_packages(),
     install_requires=[
         'pandas',
