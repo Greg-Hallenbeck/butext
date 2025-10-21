f81ccc69d83b36b0f15c74f8581bc7197fdd7976
Billy Harris <billythecorndog@gmail.com>
2025-10-21 16:19:12 -0400

    version bump

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='butext',
-    version='0.3.4',
-    description='A collection of functions related to Lexical Analysis',
+    version='0.3.5',
+    description='https://butext.readthedocs.io/en/latest/',
     long_description="placeholder",
     packages=find_packages(),
     install_requires=[
